baa1b1c9f729e30691f3bada5287ff2623298b23
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-03-24 17:05:58 +0800

    Update Olympic	Costs.py

--- Practice6/Olympic	Costs.py
+++ Practice6/Olympic	Costs.py
@@ -1,5 +1,6 @@
 #import numpy and matplotlib
 #create a dict and add values and keys 
+#use zip function to combine dorresponding keys and values as a tuple
 #use sorted fuction to sort the values and print it
 #use matplotlib to create a bar chart with determined ind width
 #add y label xtick,ytick,
@@ -8,23 +9,31 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-game_costs= {'Los_Angeles_1984': 1,
-            'Seoul_1988':8,
-            'Barcelona_1992':15,
-            'Atlanta_1996': 7,
-            'Sydney_2000':5,
-            'Athens_2003': 14,
-            'Beijing_2008':43,
-            'London_2012':40}
-costs = game_costs.values()
-print(sorted(costs))#sort the values
+game_costs= {1:'Los_Angeles_1984',
+            8:'Seoul_1988',
+            15:'Barcelona_1992',
+            7:'Atlanta_1996',
+            5:'Sydney_2000',
+            14:'Athens_2004',
+            43:'Beijing_2008',
+            40:'London_2012'}
+costs = game_costs.keys()
+games = game_costs.values()
+print(sorted(zip(costs,games)))#sort the zipped costs and games
+sc = sorted(costs) 
+
 N = 8
 ind = np.arange(N)
 width = 0.34
+colors = ['blue','green','red']
+plt.bar(ind,sc,width,color = colors)
+plt.ylabel('costs') #add y label
 
-plt.bar(ind,costs,width)
-plt.ylabel('costs')
 plt.title('Olympic_games\' costs')
-plt.xticks(ind,game_costs.keys())
+sorted_game = []
+for i in sc:
+    sorted_game.append(game_costs[i])#sort the games
+plt.xticks(ind,sorted_game)
+
 plt.yticks(np.arange(0,45,5))
 plt.show()
