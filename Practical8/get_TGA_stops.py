36867c1632f09050829d1ae9d5c7fe7cb4c3ab6f
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-04-12 14:11:59 +0800

    Update get_TGA_stops.py

--- Practical8/get_TGA_stops.py
+++ Practical8/get_TGA_stops.py
@@ -6,19 +6,20 @@ output_file = open('D:/TGA_genes.fa','w')
 
 find_gene = ''
 find_sequence = ''           
+#make two empty strings
 for line in input_file:
-    line = line.strip()
-    if line.startswith('>'):
-        if find_sequence.endswith('TGA'):
+    line = line.strip()#get the text in the file
+    if line.startswith('>'): #judge if the line starts with >, which suggests that it is a line that contains the name
+        if find_sequence.endswith('TGA'): # judge if the line ends with the TGA
             output_file.write(f">{find_gene}\n{find_sequence}\n")
-        find_gene = line[1:].split(' ')[0]
+        find_gene = line[1:].split(' ')[0]#split the things by space
         find_sequence = ''
     else:
         find_sequence += line
         
 if find_sequence.endswith('TGA'):
     output_file.write(f">{find_gene}\n{find_sequence}\n")
-    
+#find the last line
     
     
     
