37bc1250b99d931c4ac9f7a6dd57e4ff0dc8b9cf
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-04-12 14:15:05 +0800

    Update cds_counter.py

--- Practical8/cds_counter.py
+++ Practical8/cds_counter.py
@@ -4,3 +4,4 @@ seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
 stop = re.compile(r'TAA|TAG|TGA')
 stop_position = re.finditer(stop,seq)
 print(len([u.start() for u in stop_position]))
+# print the length of the list
