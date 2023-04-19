b6c5fd5d1dfa8873222b0511be78de2b54e04fe3
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-04-19 13:26:13 +0800

    Update breed_like_rabbits.py

--- Practice5/breed_like_rabbits.py
+++ Practice5/breed_like_rabbits.py
@@ -1,8 +1,8 @@
 n = 2
 g = 1
 while n <= 100:
-    n = 2 ** (g+1)
-    g += 1
+    n = 2 ** (g+1) # n means the number of the total rabbits
+    g += 1 # g refers to the generation
 print(g)
 
 # pseudocode: repeat n * 2 and g += 1 until n > 100, print(g)
