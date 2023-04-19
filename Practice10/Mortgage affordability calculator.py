15553123330c3b75b8a3edad08d444e47e12d103
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-04-19 13:37:58 +0800

    Update Mortgage affordability calculator.py

--- Practice10/Mortgage affordability calculator.py
+++ Practice10/Mortgage affordability calculator.py
@@ -1,5 +1,5 @@
 def buy_house(salary:int,worth:int):
-    if salary * 5 >= worth:
+    if salary * 5 >= worth: #judge if the 5 times of the year salary is bigger than the worth of the house
         print('you can buy the house')
-    else:
+    else: # if it is not larger
         print('you are not recommended to buy thee house')
