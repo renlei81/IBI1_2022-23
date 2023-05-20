a21ad0b265bfc06906f8e24d03964a8ee959d899
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-05-21 04:37:54 +0800

    Add files via upload

---
+++ Practical10/Protein-coding capability calculator.py
@@ -0,0 +1,20 @@
+def is_protein_coding(dna_sequence):
+    dna_sequence = dna_sequence.upper()
+    start_codon = 'ATG'
+    stop_codon = 'TGA'
+    total_length = len(dna_sequence)
+    start_index = dna_sequence.find(start_codon)
+    stop_index = dna_sequence.rfind(stop_codon)
+
+    if start_index == -1 or stop_index == -1:#judge if there are the stop or start factors
+        return 0, 'unclear'
+
+    coding_length = stop_index - start_index + len(stop_codon)# the lenght of the coding part
+    coding_percentage = coding_length / total_length
+
+    if coding_percentage > 0.5: # judge the percentage
+        return coding_percentage * 100, 'protein-coding'
+    elif coding_percentage < 0.1:
+        return coding_percentage * 100, 'non-coding'
+    else:
+        return coding_percentage * 100, 'unclear'
