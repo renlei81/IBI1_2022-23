a487d15d320d5ff48c7f8f9b79e445793566abd5
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-05-21 04:09:10 +0800

    Update cds_counter.py

--- Practical8/cds_counter.py
+++ Practical8/cds_counter.py
@@ -1,7 +1,9 @@
 import re
-seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
+seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA' #imput the possible sequence
 
-stop = re.compile(r'TAA|TAG|TGA')
-stop_position = re.finditer(stop,seq)
-print(len([u.start() for u in stop_position]))
-# print the length of the list
+stop = re.compile(r'TAA|TAG|TGA') #possible stop possitions
+
+stop_position = re.finditer(stop,seq) #find stop codon in the seq
+
+print(len([u.start() for u in stop_position])) 
+# print the length of the list, which contains all results of the finditer
