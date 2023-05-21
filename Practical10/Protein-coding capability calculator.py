063067266b1029973d7060e217d741a2ec612a7e
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-05-21 14:36:49 +0800

    Update Protein-coding capability calculator.py

--- Practical10/Protein-coding capability calculator.py
+++ Practical10/Protein-coding capability calculator.py
@@ -1,20 +1,23 @@
-def is_protein_coding(dna_sequence):
+import re
+def protein_coding(dna_sequence):
     dna_sequence = dna_sequence.upper()
-    start_codon = 'ATG'
-    stop_codon = 'TGA'
+    start_codon = r'ATG'
+    stop_codon = r'TGA'
     total_length = len(dna_sequence) #store the length of the DNA
-    start_index = dna_sequence.find(start_codon)
-    stop_index = dna_sequence.rfind(stop_codon)
+    start_index = dna_sequence.find(start_codon,re.I)
+    stop_index = dna_sequence.rfind(stop_codon,re.I)
 
     if start_index == -1 or stop_index == -1:#judge if there are the stop or start factors
         return 0, 'unclear'
 
     coding_length = stop_index - start_index + len(stop_codon)# the length of the coding sequence in the dna
-    coding_percentage = coding_length / total_length
+    coding_percentage = 100*coding_length / total_length 
 
     if coding_percentage > 0.5: # calculate the percentage of the coding sequence in the dna
-        return coding_percentage * 100, 'protein-coding' #define the types
+        return f'{coding_percentage}%', 'protein-coding' #define the types
     elif coding_percentage < 0.1:
-        return coding_percentage * 100, 'non-coding'
+        return f'{coding_percentage}%', 'non-coding'
     else:
-        return coding_percentage * 100, 'unclear'
+        return f'{coding_percentage}%', 'unclear'
+    
+print(protein_coding('aaaaatGaaaatGAa')) 
