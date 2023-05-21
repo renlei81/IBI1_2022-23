da8caf88c644942ce6fefce1e1d6f6ba283cca84
renlei81 <126224261+renlei81@users.noreply.github.com>
2023-05-21 15:17:42 +0800

    Update cds_counter.py

--- Practical9/cds_counter.py
+++ Practical9/cds_counter.py
@@ -1,7 +1,10 @@
 import re
+# input sequence
 seq = 'ATGCAATCGACTACGATCTGAGAGGGCCTAA'
-
+# define three stop codons to be found
 stop = re.compile(r'TAA|TAG|TGA')
+#find possible stop codons
 stop_position = re.finditer(stop,seq)
+# because it used re.finditer, which will not output a list,so just use a for loop to find all start positions and put them in a list
 print(len([u.start() for u in stop_position]))
 # print the length of the list
